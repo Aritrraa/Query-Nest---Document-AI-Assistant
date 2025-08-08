4d200574c42627764f2f516eb81b8e7a94941879
Aritra Das <146645466+Aritrraa@users.noreply.github.com>
2025-08-09 00:50:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,10 +161,11 @@ def delete_files():
 # Run the app
 
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
-    app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    app.run(host='0.0.0.0', port=port)
+
 
 
 
