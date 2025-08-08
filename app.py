c0bd3bfbd37a6912928d139aa6f9c09b330f3b36
Aritra Das <146645466+Aritrraa@users.noreply.github.com>
2025-08-08 23:54:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -158,6 +158,11 @@ def delete_files():
     return jsonify({'message': 'All uploaded files deleted successfully.'})
 
 # Run the app
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
 
